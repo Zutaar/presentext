45bdb6338fe3103fb2e01d766d2ff44f3161d120
Zutaar <leon.falvey@gmail.com>
2016-11-03 10:34:30 +1100

    user can now create an account using signup

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 # For local hosting and testing
 require 'sinatra/reloader'
 require 'pg'
+# for testing single commands
+require 'pry'
 
 require_relative 'db_config'
 require_relative 'models/user'
 require_relative 'models/slideshow'
 require_relative 'models/slide'
+
+# binding.pry
 
 enable :sessions
 
@@ -21,18 +25,38 @@ helpers do
 end
 
 get '/' do
-  if !logged_in?
-    erb :index
-  else
+  if logged_in?
 
     erb :user_home
+
+  else
+
+    erb :index
+
+  end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  new_user = User.new(email: params[:email], password: params[:password])
+
+  if new_user.save
+    session[:user_id] = new_user.id
+
+    redirect to '/'
+  else
+
+    erb :signup
   end
 end
 
 get '/login' do
-
+  erb :login
 end
 
-get '/signup' do
-  
+post '/login' do
+
 end
